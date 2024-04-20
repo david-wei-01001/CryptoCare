aadae179219de956e8570cc3de365c40612ec143
davidatuken <72998341+david-wei-01001@users.noreply.github.com>
2024-04-20 18:50:22 -0400

    added signout

--- frontend/src/components/NavigationSideBar/NavigationSidebar.js
+++ frontend/src/components/NavigationSideBar/NavigationSidebar.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react';
 import './NavigationSidebar.css';
 import NavbarLinks from './NavbarLinks';
 import { useUser } from '../contexts/UserContext.js';
-import { firestore } from '../FireBase/firebase.js';
+import { auth, firestore } from '../FireBase/firebase.js';
 import { doc, getDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
 
 const NavigationSidebar = () => {
   const user = useUser();  // Access user data from the context
   const [firstName, setFirstName] = useState('');  // State to store the first name
   const [lastName, setLastName] = useState('');  // State to store the last name
+  const navigate = useNavigate();
 
 
   useEffect(() => {
@@ -37,6 +39,20 @@ const NavigationSidebar = () => {
     }
   }, [user?.uid]);  // Dependency array to re-fetch if user.uid changes
 
+  const signOutUser = () => {
+    auth.signOut().then(() => {
+      console.log("User signed out successfully");
+      // Optionally redirect to the login page or home page after signing out
+    }).catch((error) => {
+      console.error("Sign out error", error);
+    });
+  };
+
+  const handleSignOut = () => {
+    signOutUser();  // call the sign-out function
+    navigate('/');  // navigate to login page
+  };
+
   return (
     <div className="sidebar">
        <div className="nav-links">
@@ -51,13 +67,23 @@ const NavigationSidebar = () => {
 
         <NavbarLinks className="link-container" page="Wallet" img={`${process.env.PUBLIC_URL}/walletInactive.svg`} activeImg={`${process.env.PUBLIC_URL}/walletActive.svg`} />
 
+        
+        
         <div className="profile-info">
-          <div className="navbar-link">
+          <div className="navbar-l">
             <img src={`${process.env.PUBLIC_URL}/profileLogo.svg`}  alt="Logo" className="navbar-logo" />
             {/* Dynamically display the user's first and last name or a placeholder if not available */}
             <span className="navbar-text">{firstName && lastName ? `${firstName} ${lastName}` : 'Loading...'}</span>
           </div>
         </div>
+
+        {/* Sign Out Button */}
+        <div className="sign-out-container" style={{padding: '10px'}}>
+          <button onClick={handleSignOut} className="sign-out-button">
+            Sign Out
+          </button>
+        </div>
+
       </div>
     </div>
   );
