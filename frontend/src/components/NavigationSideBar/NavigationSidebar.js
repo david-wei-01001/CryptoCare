c12ab22f9a23fe18882095877f4392ee92d6c9e1
Sejung <49921709+sejungk@users.noreply.github.com>
2024-04-20 10:14:35 -0400

    fix: updated image urls on side navbar

--- frontend/src/components/NavigationSideBar/NavigationSidebar.js
+++ frontend/src/components/NavigationSideBar/NavigationSidebar.js
@@ -7,17 +7,17 @@ const NavigationSidebar = () => {
     <div className="sidebar">
        <div className="nav-links">
         <div className="navbar-link app-logo">
-          <img src='./projectLogo.svg' alt="Logo" className="navbar-logo" />
+          <img src={`${process.env.PUBLIC_URL}/projectLogo.svg`} alt="Logo" className="navbar-logo" />
           <span className="navbar-text">APP NAME</span>
         </div>
 
-        <NavbarLinks page="Dashboard" img = './dashboardActive.svg'/>
-        <NavbarLinks page="Charities" img = './charitiesActive.svg' />
-        <NavbarLinks page="Wallet" img = './walletActive.svg'/>
+        <NavbarLinks page="Dashboard" img={`${process.env.PUBLIC_URL}/dashboardActive.svg`} />
+        <NavbarLinks page="Charities" img={`${process.env.PUBLIC_URL}/charitiesActive.svg`} />
+        <NavbarLinks page="Wallet" img={`${process.env.PUBLIC_URL}/walletActive.svg`} />
 
         <div className="profile-info">
           <div className="navbar-link">
-            <img src='./profileLogo.svg'  alt="Logo" className="navbar-logo" />
+            <img src={`${process.env.PUBLIC_URL}/profileLogo.svg`}  alt="Logo" className="navbar-logo" />
             <span className="navbar-text">Stephen Flores</span>
           </div>
         </div>
