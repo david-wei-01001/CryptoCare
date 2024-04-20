6ae1c04f6043991a18089a0dfad17d4883a2323f
Sejung <49921709+sejungk@users.noreply.github.com>
2024-04-20 13:49:47 -0400

    style: Added active link highlighting on navbar

--- frontend/src/components/NavigationSideBar/NavbarLinks.js
+++ frontend/src/components/NavigationSideBar/NavbarLinks.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './NavigationSidebar.css';
 
-const NavbarLink = ({ page, img }) => {
+const NavbarLink = ({ page, img, activeImg }) => {
+  const { pathname } = useLocation();
+  const isActive = pathname === `/${page.toLowerCase()}`;
+
   let pageUrl = `/${page.toLowerCase()}`
   return (
-    <Link to={pageUrl} className="navbar-link">
-      <img src={img} alt="Logo" className="navbar-logo" />
+    <Link to={pageUrl} className={`navbar-link ${isActive ? 'active' : ''}`}>
+      <img src={isActive ? activeImg : img} alt={page}className="navbar-logo" />
       <span className="navbar-text">{page}</span>
     </Link>
   );
