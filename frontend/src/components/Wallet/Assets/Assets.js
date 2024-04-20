397b0b07666b3c241c96fb078997b53f4a9649a0
Sejung <49921709+sejungk@users.noreply.github.com>
2024-04-20 05:05:34 -0400

    feat: Created Wallet Card Component

---
+++ frontend/src/components/Wallet/Assets/Assets.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Assets.css';
+
+const Assets = () => {
+  return (
+    <div className="assets">
+Assets
+    </div>
+  );
+};
+
+export default Assets;
--- frontend/src/components/Wallet/Wallet.js
+++ frontend/src/components/Wallet/Wallet.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import NavigationSidebar from '../NavigationSideBar/NavigationSidebar';
+import Assets from './Assets/Assets';
+import WalletCard from './WalletCard/WalletCard';
+import './Wallet.css';
 
 const Wallet = () => {
   return (
-    <div>
-      <h1>Wallet</h1>
+    <div className="wallet-container">
+      <div className="sidebar">
+        {<NavigationSidebar />}
+      </div>
+      <div className="wallet-content">
+        <div className="top-section">
+          <h1 className="header">Wallet</h1>
+          <WalletCard />
+        </div>
+
+
+        <div className="bottom-section">
+          <div className="assets-section">
+            <div className="header-section">
+              <h1 className="header">Featured Charities</h1>
+            </div>
+            <Assets />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
