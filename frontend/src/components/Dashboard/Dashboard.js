d6bff6fd52a2befd2e1e6576b1b4ae7abcec788c
Sejung <49921709+sejungk@users.noreply.github.com>
2024-04-20 02:04:15 -0400

    feat: Create initial layout for the dashboard

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Dashboard.css';
 import NavigationSidebar from '../NavigationSideBar/NavigationSidebar';
+import Donations from './Donations';
+import FeaturedCharities from './FeaturedCharities';
 
 const Dashboard = () => {
   return (
@@ -8,9 +10,27 @@ const Dashboard = () => {
       <div className="sidebar">
         {<NavigationSidebar />}
       </div>
-      <div className="content">
-        <h1>Welcome, Stephan</h1>
+      <div className="dashboard-content">
+        <div className="top-section">
+          <div className="donations-card">
+            <h1 className="header">Welcome, Stephan</h1>
+            <Donations />
+          </div>
+
+          <div className="donations-card">
+            <h1 className="header">Donation History</h1>
+            <Donations />
+          </div>
+        </div>
+
+        <div className="bottom-section">
+          <div className="featured-charities">
+            <h1 className="header">Featured Charities</h1>
+            <FeaturedCharities />
+          </div>
+        </div>
       </div>
+
     </div>
   );
 };
---
+++ frontend/src/components/Dashboard/Donations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Dashboard.css';
+
+const Donations = () => {
+  return (
+    <div className="donations-container">
+      <div>Donations</div>
+    </div>
+  );
+};
+
+export default Donations;
