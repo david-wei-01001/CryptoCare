5795b86015fbeeb06051ec8bada1b240b7fb8bb2
Sejung <49921709+sejungk@users.noreply.github.com>
2024-04-20 04:12:11 -0400

    feat: Added Donation History Card for Empty Histories

---
+++ frontend/src/components/Dashboard/DonationHistory/DonationHistoryEmptyCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './DonationHistory.css';
+
+const DonationHistoryEmptyCard = () => {
+  return (
+    <div className="donation-history-card empty">
+      <p>Make a donation to view them here.</p>
+    </div>
+  );
+};
+
+export default DonationHistoryEmptyCard;
+
+
