ad868462bec6fd6af90f1bea87c061ed342aceb7
davidatuken <72998341+david-wei-01001@users.noreply.github.com>
2024-04-20 01:28:05 -0400

    place holder, changed some style

--- frontend/src/components/Auth/Login/Login.js
+++ frontend/src/components/Auth/Login/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { auth } from '../../FireBase/firebase.js';
 import './Login.css';
 
@@ -9,9 +11,14 @@ function Login() {
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');  // To handle and display errors
   const [successMessage, setSuccessMessage] = useState(''); // State to store success message
-
+  const [showPassword, setShowPassword] = useState(false); // Define showPassword state
   const navigate = useNavigate();
 
+  // Function to toggle password visibility
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -53,36 +60,89 @@ function Login() {
     }
   };
   return (
-    <div className="Login">
-      {/* Display error and success messages */}
-      {errorMessage && <div className="error-message">{errorMessage}</div>}
-      {successMessage && <div className="success-message">{successMessage}</div>}
-      
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+    <div className="login-container">
+      <div className="left-section">
+        <h1>Charity Bitcoin App</h1>
+        <div className="logo">
+          {/* Your logo or image goes here */}
+          <img src="../../../../public/logo512.png" alt="Logo" />
+          
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+      </div>
+      <div className="right-section">
+        <div className="form-container">
+          <div className="form-header">
+            <h2>Login</h2>
+            {/* Display error and success messages */}
+            {errorMessage && <div className="error-message">{errorMessage}</div>}
+            {successMessage && <div className="success-message">{successMessage}</div>}
+          </div>
+          <div className="form-body">
+            <form onSubmit={handleSubmit}>
+            <div className="input-container">
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="input-container">
+              <div className="password-input">
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder='Enter your password'
+                  required
+                />
+                <div className="password-toggle" onClick={togglePasswordVisibility}>
+                  <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+                </div>
+              </div>
+            </div>
+            <button type="submit">Log in</button>
+            </form>
+            <p>Don't have an account? <Link to="/register">Sign up</Link></p>
+          </div>
         </div>
-        <button type="submit">Log in</button>
-      </form>
-      <p>Don't have an account?<Link to="/register">Sign up</Link></p>
+      </div>
     </div>
+
+
+
+
+    // <div className="Login">
+    //   {/* Display error and success messages */}
+    //   {errorMessage && <div className="error-message">{errorMessage}</div>}
+    //   {successMessage && <div className="success-message">{successMessage}</div>}
+      
+    //   <form onSubmit={handleSubmit}>
+      //   <div>
+      //     <label htmlFor="email">Email:</label>
+      //     <input
+      //       type="email"
+      //       id="email"
+      //       value={email}
+      //       onChange={(e) => setEmail(e.target.value)}
+      //       required
+      //     />
+      //   </div>
+      //   <div>
+      //     <label htmlFor="password">Password:</label>
+      //     <input
+      //       type="password"
+      //       id="password"
+      //       value={password}
+      //       onChange={(e) => setPassword(e.target.value)}
+      //       required
+      //     />
+      //   </div>
+      //   <button type="submit">Log in</button>
+      // </form>
+      // <p>Don't have an account?<Link to="/register">Sign up</Link></p>
+    // </div>
   );
 }
 
